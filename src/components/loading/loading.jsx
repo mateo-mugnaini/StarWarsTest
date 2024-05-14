e27df3a781f067b84ac52d371ade040797a10e74
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2024-05-14 11:40:11 -0300

    refactor(-): Cambios en el loading

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className='flex flex-row w-full gap-4 items-center justify-center font-nunito text-2xl'>
-      <hr className='border-4 border-gray-300 border-t-4 border-blue-500 rounded-full w-12 h-12 animate-spin'></hr>
-      Cargando...
+    <div className='flex flex-row w-full gap-5 items-center justify-center font-nunito text-2xl'>
+      <hr className='border-5 border-gray-300 border-t-5 border-blue-500 rounded-full w-12 h-12 animate-spin'></hr>
+      Loading
     </div>
   );
 };
